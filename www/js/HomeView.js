0c8b72ff23a61b00a18fc7c63d852f6abc6be5bc
David Meredith <david@meredithdev.com>
2013-03-05 15:37:59 -0800

    Prep For Phonegap build

---
+++ www/js/HomeView.js
@@ -0,0 +1,40 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: dmeredith1
+ * Date: 3/5/13
+ * Time: 12:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var HomeView = function(store){
+    this.initialize = function(){
+        // Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+        this.el.on('keyup','.search-key', this.findByName);
+    };
+
+    this.render = function(){
+        this.el.html(HomeView.template());
+        return this;
+    };
+
+    this.findByName = function(){
+        store.findByName($('.search-key').val(),function(employees){
+            $('.employee-list').html(HomeView.liTemplate(employees));
+            if(self.iScroll){
+                console.log('Refresh iScroll');
+                self.iScroll.refresh();
+            }
+            else
+            {
+                console.log('New iScroll');
+                self.iScroll = new iScroll($('.scroll',self.el)[0],{hScrollbar:false, vScrollbar:false});
+            }
+        });
+    };
+
+    this.initialize();
+}
+
+HomeView.template = Handlebars.compile($('#home-tpl').html());
+HomeView.liTemplate = Handlebars.compile($('#employee-li-tpl').html());
+
