1efe15f47cef2e241963bced41b9c4429321abd2
David Meredith <david@meredithdev.com>
2013-03-05 11:57:02 -0800

    Test

--- js/main.js
+++ js/main.js
@@ -2,22 +2,41 @@ var app = {
 
     findByName: function() {
         console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    showAlert: function(message, title){
+        if (navigator.notification){
+            navigator.notification.alert(message,null,title,'OK');
+        }
+        else{
+            alert(title ?(title + ": " + message) : message);
+        }
+    },
 
+    renderHomeView: function(){
+       var html =
+           "<div class='header'><h1>Home</h1></div>" +
+               "<div class='search-view'>" +
+               "<input class='search-key'/>" +
+               "<ul class='employee-list'></ul>" +
+               "</div>";
+        this.homeTpl = Handlebars.compile($('#home-tpl').html());
+        this.employeeLiTpl = Handlebars.compile($('#employee-li-tpl').html());
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    initialize: function() {
+        var self = this;
+        this.store = new MemoryStore(function(){
+           self.renderHomeView();
+        });
+
+    }
 };
 
 app.initialize();
